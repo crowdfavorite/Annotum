9263fd20415ccdbf6496458ec7438e0f34b7d374
Evan Anderson <ejdanderson@gmail.com>
2012-08-22 11:27:15 -0600

    Authors in the of articles, recently categories widget

---
+++ jst/assets/load.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package anno
+ * This file is part of the Annotum theme for WordPress
+ * Built on the Carrington theme framework <http://carringtontheme.com>
+ *
+ * Copyright 2008-2011 Crowd Favorite, Ltd. All rights reserved. <http://crowdfavorite.com>
+ * Released under the GPL license
+ * http://www.opensource.org/licenses/gpl-license.php
+ */
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+
+
+wp_enqueue_style('jst', JST_URL.'assets/css/main.css', array(), JST_VER, 'screen, print');
--- jst/functions.php
+++ jst/functions.php
@@ -2,23 +2,20 @@
 
 
 @define('JST_PATH', trailingslashit(get_stylesheet_directory()));
+@define('JST_URL', trailingslashit(get_stylesheet_directory_uri()));
+@define('JST_VER', '1.0');
 
 
 // WP will include this file by default but it needs to be loaded prior to the parent themes versions
 include_once JST_PATH.'plugins/load.php'; 
 
-
-/**
- * Get the number of authors for an article via the snapshot.
- * @param int post_id ID of the post to get the number from 
- * @return Number of authors, 1 if no snapshot found (default WP)
- **/
-function annojst_num_authors($post_id) {
-	$authors = get_post_meta($post_id, '_anno_author_snapshot', true);
-	if (is_array($authors)) {
-		return count($authors);
-	}
-
-	// Default WP, only one author
-	return 1;
+function annojst_widgets_init() {
+	include_once(JST_PATH . 'functions/JST_Widget_Recently_Categories.php');
+	register_widget('JST_Widget_Recently_Categories');
 }
+add_action('widgets_init', 'annojst_widgets_init');
+
+function annojst_assets() {
+	cfct_template_file('assets', 'load');
+}
+add_action('wp_enqueue_scripts', 'annojst_assets');
